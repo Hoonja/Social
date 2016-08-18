ba6fa418ad63178e907df19caa3623e6e3ce592a
CHOIJAE HOON <hoonja@gmail.com>
2016-08-18 15:09:41 +0900

    ..

--- www/js/app.js
+++ www/js/app.js
@@ -22,3 +22,82 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+.factory('fbService', ['$q', function ($q) {
+  return {
+    getLoginStatus: function() {
+      var deferred = $q.defer();
+      FB.getLoginStatus(function(response) {
+        console.log('FB.getLoginStatus', response);
+        if (response.status === 'connected') {
+          deferred.resolve();
+        } else if (response.status === 'not_authorized') {
+          deferred.reject('not_authorized');
+        } else {
+          deferred.reject(response.status);
+        }
+      })
+
+      return deferred.promise;
+    },
+    login: function() {
+      var deferred = $q.defer();
+
+      FB.login(function(response) {
+        console.log('FB.login', response);
+        if (response.authResponse) {
+          console.log('Welcome!  Fetching your information.... ');
+          FB.api('/me', function(response) {
+            console.log('Good to see you, ' + response.name + '.');
+            deferred.resolve();
+           });
+        } else {
+         console.log('User cancelled login or did not fully authorize.');
+         deferred.reject();
+        }
+      });
+
+      return deferred.promise;
+    },
+    logout: function() {
+      var deferred= $q.defer();
+      FB.logout(function(response) {
+        console.log('FB.logout', response);
+        deferred.resolve();
+      });
+      return deferred.promise;
+    }
+  };
+}])
+.controller('main', ['$scope', 'fbService', function($scope, fbService) {
+  console.log('controller');
+  $scope.isLoggedIn = false;
+
+  checkLoginState = function() {
+    fbService.getLoginStatus()
+    .then(function() {
+      $scope.isLoggedIn = true;
+    }, function(err) {
+      $scope.isLoggedIn = false;
+    })
+  };
+
+  setTimeout(function() {
+    checkLoginState();
+  }, 1000);
+
+  $scope.login = function() {
+    fbService.login()
+    .then(function() {
+      $scope.isLoggedIn = true;
+    }, function() {
+      $scope.isLoggedIn = false;
+    });
+  };
+
+  $scope.logout = function() {
+    fbService.logout()
+    .then(function() {
+      $scope.isLoggedIn = false;
+    });
+  };
+}]);
